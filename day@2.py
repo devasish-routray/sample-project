8f4b4e1f1b32c3ac39fc9cd0167daeca33368d63
devasish-routray <devasishroutray@outlook.com>
2024-04-01 18:54:27 +0530

    new code

---
+++ day@2.py
@@ -0,0 +1,9 @@
+# This program calculates the factorial of a number entered by the user.
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+num = int(input("Enter a number: "))
+print("Factorial of", num, "is", factorial(num))
