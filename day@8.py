dbfc98b4956e85ed007d8bc56c4304ffcc005d88
devasish-routray <devasishroutray@outlook.com>
2024-04-09 00:02:40 +0530

    new code

---
+++ day@8.py
@@ -0,0 +1,9 @@
+#  user to enter two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calculating the sum of the two numbers
+sum = num1 + num2
+
+# Displaying the result
+print("The sum of", num1, "and", num2, "is:", sum)
