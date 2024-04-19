69f4a1a81476790b6e1df7a2aa54ef65d056ae40
devasish-routray <devasishroutray@outlook.com>
2024-04-19 22:17:25 +0530

    aera of rectangle

--- day@4.py
+++ day@4.py
@@ -10,4 +10,4 @@ width = float(input("Enter the width of the rectangle: "))
 area = calculate_rectangle_area(length, width) 
 
 # Print the result
-print("The area of the rectangle is:", area)
+print("The area of the rectangle is:", area)  
